f64b756c50c137cfa52d279d1f7507a8a6c247c7
Pedro Giuglio <pedrogiuglio@hotmail.com>
2023-05-15 21:56:35 -0300

    bug on click fixed

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-// const titulo = document.querySelector(".titulo");
-// titulo.style.color = "red";
-
-// const sellosEstilos = {
-//     width: "300px",
-//     height: "150px",
-//     backgroundColor: "red",
-//   };
-  
-//   const selloAgua = document.createElement("div");
-//   selloAgua.style.width = sellosEstilos.width;
-//   selloAgua.style.height = sellosEstilos.height;
-//   selloAgua.style.backgroundColor = sellosEstilos.backgroundColor;
-//   document.body.appendChild(selloAgua);
-  
-//   const selloCloaca = document.createElement("div");
-//   selloCloaca.style.width = sellosEstilos.width;
-//   selloCloaca.style.height = sellosEstilos.height;
-//   selloCloaca.style.backgroundColor = sellosEstilos.backgroundColor;
-//   selloCloaca.style.marginTop = "20px";
-//   document.body.appendChild(selloCloaca);
-
-// function CalcularPropina () {
-//  let totalFactura = parseFloat(prompt("Ingrese aquí el monto total:"));
-//  let porcentajePropina = parseFloat(prompt("Ingrese porentaje de propina:"));
-
-//  let montoPropina = totalFactura * (porcentajePropina / 100);
-//  let totalConPropina = totalFactura + montoPropina
-//  alert(`Monto de la propina: $${montoPropina.toFixed(2)}`)
-//  alert(`Total a pagar (con propina): $${totalConPropina.toFixed(2)}`);
-// }
- 
-// CalcularPropina();
-
 function calcularPropina () {
   let totalFacturaInput = document.getElementById('totalFactura')
   let porcentajePropinaInput = document.getElementById('porcentajeDePropina')
